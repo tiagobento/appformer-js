2dc672d445ff1b35f0b198b83504b5141134309a
Tiago Bento <tiagobentofernandes@gmail.com>
2019-03-19 18:29:42 -0300

    Improve index again

--- packages/submarine-showcase/src/index.tsx
+++ packages/submarine-showcase/src/index.tsx
@@ -2,10 +2,11 @@ import { JsonEditor } from "./JsonEditor";
 import * as AppFormer from "appformer-js-core";
 import { AppFormerSubmarine } from "appformer-js-submarine";
 
-function asyncLoadEditor(appFormer: AppFormerSubmarine) : Promise<AppFormer.Editor>{
-  return new Promise(res => setTimeout(() => appFormer.registerEditor(JsonEditor).then(res), 1200));
+//This function can be replaced by an async import
+function asyncLoadEditorModule(): Promise<{ Editor: { new (): AppFormer.Editor } }> {
+  return new Promise(res => setTimeout(() => res({ Editor: JsonEditor }), 1200));
 }
 
 AppFormerSubmarine.init(document.getElementById("app")!)
-  .then(appFormer => asyncLoadEditor(appFormer))
+  .then(appFormer => asyncLoadEditorModule().then(editorModule => appFormer.registerEditor(editorModule.Editor)))
   .then(editor => editor.setContent(`{"foo": "this is a test"}`));
