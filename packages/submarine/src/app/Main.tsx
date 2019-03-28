26f37eaca3c63828f331bc9bc7ede600b949a91b
Tiago Bento <tiagobentofernandes@gmail.com>
2019-03-28 11:07:09 -0400

    Prepare for demo

--- packages/submarine/src/app/Main.tsx
+++ packages/submarine/src/app/Main.tsx
@@ -8,7 +8,17 @@ export function Main(props: { editor?: AppFormer.Editor }) {
       <section>
         <div>
           {props.editor && <ComponentWrapper key={props.editor.af_componentId} component={props.editor} />}
-          {!props.editor && <span>No Editors registered...</span>}
+          {!props.editor && (
+            <div style={{ textAlign: "center", width: "100vw", height: "100vh", backgroundColor: "black" }}>
+              <br />
+              <br />
+              <br />
+              <img
+                width={"20px"}
+                src={"https://upload.wikimedia.org/wikipedia/commons/3/37/YouTube_loading_symbol_2_%28stable%29.gif"}
+              />
+            </div>
+          )}
         </div>
       </section>
     </main>
