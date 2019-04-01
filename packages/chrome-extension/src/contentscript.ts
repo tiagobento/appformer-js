54330ca8a49ec745c68580167109d2f688a1cdb8
Tiago Bento <tiagobentofernandes@gmail.com>
2019-04-01 18:17:12 -0400

    Improved code

--- packages/chrome-extension/src/contentscript.ts
+++ packages/chrome-extension/src/contentscript.ts
@@ -21,81 +21,95 @@ function enableCommitButton() {
   commitButton.removeAttribute("disabled");
 }
 
+function insertInGitHubPage(...elements: HTMLElement[]) {
+  const targetContainer = document.querySelector(".file");
+  elements.forEach(e => {
+    targetContainer!.parentElement!.insertBefore(e, targetContainer);
+    targetContainer!.parentElement!.insertBefore(e, targetContainer);
+  });
+}
+
 function init() {
   const githubEditor = document.querySelector(".js-code-editor") as HTMLElement;
+  if (!githubEditor) {
+    console.info("Not GitHub.");
+    return;
+  }
 
+  const splitLocationHref = window.location.href.split(".");
+  const openFileLanguage = splitLocationHref[splitLocationHref.length - 1];
+  if (!router.has(openFileLanguage)) {
+    console.info(`No enhanced editor available for "${openFileLanguage}" format.`)
+    return;
+  }
+
+  //**!!!!**
+  //REMEMBER: open /Applications/Google\ Chrome.app --args --allow-running-insecure-content
+  //**!!!!**
+
+  //Sanity check
+  console.log("Extension is running! 4");
+
+  //Bootstrap GitHub native editor
+  //FIXME: Do we really have to do this?
   CodeMirror(document.body);
   CodeMirror.fromTextArea(document.querySelector(".file-editor-textarea"));
 
+  const iframeDomain = "http://localhost:9000";
+  const embeddedEditorIframe = document.createElement("iframe");
 
-  if (githubEditor) {
-    //**!!!!**
-    //REMEMBER: open /Applications/Google\ Chrome.app --args --allow-running-insecure-content
-    //**!!!!**
+  window.addEventListener(
+    "message",
+    (event: MessageEvent) => {
+      const message = event.data as AppFormerBusMessage<any>;
+      switch (message.type) {
+        case "REQUEST_LANGUAGE":
+          const languageReturnMessage = { type: "RETURN_LANGUAGE", data: router.get(openFileLanguage) };
+          embeddedEditorIframe.contentWindow!.postMessage(languageReturnMessage, iframeDomain);
+          break;
+        case "REQUEST_SET_CONTENT":
+          const githubEditorContent = getGitHubEditor().CodeMirror.getValue() || "";
+          const setContentReturnMessage = { type: "RETURN_SET_CONTENT", data: githubEditorContent };
+          embeddedEditorIframe.contentWindow!.postMessage(setContentReturnMessage, iframeDomain);
+          break;
+        case "RETURN_GET_CONTENT":
+          const gwtEditorContent = message.data;
+          enableCommitButton();
+          getGitHubEditor().CodeMirror.setValue(gwtEditorContent);
+          break;
+        default:
+          console.info("Unknown message type " + message.type);
+          break;
+      }
+    },
+    false
+  );
 
-    //Sanity check
-    console.log("Extension is running! 4");
+  //hide current editor
+  githubEditor.style.display = "none";
 
-    const iframeDomain = "http://localhost:9000";
-    const gwtiframe = document.createElement("iframe");
+  //Inserts iframe to isolate CSS and JS contexts
+  embeddedEditorIframe.id = "gwt-iframe";
+  embeddedEditorIframe.src = "http://localhost:9000";
+  embeddedEditorIframe.style.cssText = "width:100%; height:600px;";
 
-    window.addEventListener(
-      "message",
-      (event: MessageEvent) => {
-        const message = event.data as AppFormerBusMessage<any>;
-        switch (message.type) {
-          case "REQUEST_LANGUAGE":
-            const languageReturnMessage = { type: "RETURN_LANGUAGE", data: router.get("dmn") };
-            gwtiframe.contentWindow!.postMessage(languageReturnMessage, iframeDomain);
-            break;
-          case "REQUEST_SET_CONTENT":
-            const githubEditorContent = getGitHubEditor().CodeMirror.getValue() || "";
-            const setContentReturnMessage = { type: "RETURN_SET_CONTENT", data: githubEditorContent };
-            gwtiframe.contentWindow!.postMessage(setContentReturnMessage, iframeDomain);
-            break;
-          case "RETURN_GET_CONTENT":
-            const gwtEditorContent = message.data;
-            enableCommitButton();
-            getGitHubEditor().CodeMirror.setValue(gwtEditorContent);
-            break;
-          default:
-            console.info("Unknown message type " + message.type);
-            break;
-        }
-      },
-      false
-    );
+  //Insert button after default GitHub editor
+  const fullScreenButton = document.createElement("button");
+  fullScreenButton.textContent = "Fullscreen";
+  fullScreenButton.onclick = e => {
+    e.preventDefault();
+    document.body.appendChild(embeddedEditorIframe);
+    embeddedEditorIframe.style.cssText = "width:100vw;height:100vh;position:absolute;top:0px;z-index:999";
+  };
 
-    //hide current editor
-    githubEditor.style.display = "none";
+  //FIXME: Find a way to request editor content just when necessary
+  setInterval(() => {
+    enableCommitButton();
+    const requestGetContent = { type: "REQUEST_GET_CONTENT" };
+    embeddedEditorIframe.contentWindow!.postMessage(requestGetContent, iframeDomain);
+  }, 1000);
 
-    //Inserts iframe to isolate CSS and JS contexts
-    gwtiframe.id = "gwt-iframe";
-    gwtiframe.src = "http://localhost:9000";
-    gwtiframe.style.cssText = "width:100%; height:600px;";
-
-    //Insert button after default GitHub editor
-    const fullScreenButton = document.createElement("button");
-    fullScreenButton.textContent = "Fullscreen";
-    fullScreenButton.onclick = e => {
-      e.preventDefault();
-      document.body.appendChild(gwtiframe);
-      gwtiframe.style.cssText = "width:100vw;height:100vh;position:absolute;top:0;z-index:999";
-    };
-
-    //FIXME: Find a way to request editor content just when necessary
-    setInterval(() => {
-      enableCommitButton();
-      const requestGetContent = { type: "REQUEST_GET_CONTENT" };
-      gwtiframe.contentWindow!.postMessage(requestGetContent, iframeDomain);
-    }, 1000);
-
-    const targetContainer = document.querySelector(".file");
-    targetContainer!.parentElement!.insertBefore(gwtiframe, targetContainer);
-    targetContainer!.parentElement!.insertBefore(fullScreenButton, targetContainer);
-  } else {
-    console.log("not github");
-  }
+  insertInGitHubPage(embeddedEditorIframe, fullScreenButton);
 }
 
 init();
