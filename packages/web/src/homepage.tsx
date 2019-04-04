568fa16efe7ca86156b572a7d59ebc54eb232951
Sarah Rambacher <srambach@redhat.com>
2019-04-04 17:41:58 -0400

    fixes gallery view of homepage

--- packages/web/src/homepage.tsx
+++ packages/web/src/homepage.tsx
@@ -93,7 +93,7 @@ export function Homepage() {
         </Title>
       </PageSection>
       <PageSection variant="dark">
-        <Gallery gutter="md">
+        <Gallery gutter="lg">
           <GalleryItem>
             <Card>
               <CardHeader>
@@ -101,21 +101,16 @@ export function Homepage() {
               </CardHeader>
               <CardBody>
                 <TextContent className="pf-u-mb-xl">
-                <Text component={TextVariants.p}>Quickly create business diagrams using a desktop application. Store files locally using the desktop
-                  file system.
+                  <Text component={TextVariants.p}>
+                    Quickly create business diagrams using a desktop application. Store files locally using the desktop
+                    file system.
                   </Text>
-                  <Text component="p"> </Text>
+                  <Text component="p" />
                 </TextContent>
                 <List>
-                  <ListItem>
-                    Fast diagramming
-                  </ListItem>
-                  <ListItem>
-                    Free-form file organization
-                  </ListItem>
-                  <ListItem>
-                    Local file storage
-                  </ListItem>
+                  <ListItem>Fast diagramming</ListItem>
+                  <ListItem>Free-form file organization</ListItem>
+                  <ListItem>Local file storage</ListItem>
                 </List>
               </CardBody>
               <CardFooter>
@@ -129,24 +124,23 @@ export function Homepage() {
                 </Split>
               </CardFooter>
             </Card>
-
+          </GalleryItem>
+          <GalleryItem>
             <Card>
               <CardHeader>
                 <Title size="2xl">Web</Title>
               </CardHeader>
               <CardBody>
                 <TextContent className="pf-u-mb-xl">
-                <Text component={TextVariants.p}>Easily access online tooling and storage. The web experience offers direct access to online business process and decision editors.
+                  <Text component={TextVariants.p}>
+                    Easily access online tooling and storage. The web experience offers direct access to online business
+                    process and decision editors.
                   </Text>
-                  <Text component="p"> </Text>
+                  <Text component="p" />
                 </TextContent>
                 <List>
-                  <ListItem>
-                    Online access to projects and editors
-                  </ListItem>
-                  <ListItem>
-                    Quick file navigation
-                  </ListItem>
+                  <ListItem>Online access to projects and editors</ListItem>
+                  <ListItem>Quick file navigation</ListItem>
                 </List>
               </CardBody>
               <CardFooter>
@@ -160,25 +154,22 @@ export function Homepage() {
                 </Split>
               </CardFooter>
             </Card>
-
-
+          </GalleryItem>
+          <GalleryItem>
             <Card>
               <CardHeader>
                 <Title size="2xl">Browser extensions</Title>
               </CardHeader>
               <CardBody>
                 <TextContent className="pf-u-mb-xl">
-                <Text component={TextVariants.p}>Online direct access to editors in the preferred developer source code host
+                  <Text component={TextVariants.p}>
+                    Online direct access to editors in the preferred developer source code host
                   </Text>
-                  <Text component="p"> </Text>
+                  <Text component="p" />
                 </TextContent>
                 <List>
-                  <ListItem>
-                    Community focused
-                  </ListItem>
-                  <ListItem>
-                    Integrate diretly into source code host (GitHub)
-                  </ListItem>
+                  <ListItem>Community focused</ListItem>
+                  <ListItem>Integrate diretly into source code host (GitHub)</ListItem>
                 </List>
               </CardBody>
               <CardFooter>
@@ -193,28 +184,23 @@ export function Homepage() {
                 </Split>
               </CardFooter>
             </Card>
-
-
+          </GalleryItem>
+          <GalleryItem>
             <Card>
               <CardHeader>
                 <Title size="2xl">IDE extensions</Title>
               </CardHeader>
               <CardBody>
                 <TextContent className="pf-u-mb-xl">
-                <Text component={TextVariants.p}>Integrate directly with developer IDEs. Edit files directly in a developer IDE environment.
+                  <Text component={TextVariants.p}>
+                    Integrate directly with developer IDEs. Edit files directly in a developer IDE environment.
                   </Text>
-                  <Text component="p"> </Text>
+                  <Text component="p" />
                 </TextContent>
                 <List>
-                  <ListItem>
-                    Edit multiple files at a time
-                  </ListItem>
-                  <ListItem>
-                    Tree view "file explorer"
-                  </ListItem>
-                  <ListItem>
-                    Integrate directly into developer IDE
-                  </ListItem>
+                  <ListItem>Edit multiple files at a time</ListItem>
+                  <ListItem>Tree view "file explorer"</ListItem>
+                  <ListItem>Integrate directly into developer IDE</ListItem>
                 </List>
               </CardBody>
               <CardFooter>
@@ -229,7 +215,6 @@ export function Homepage() {
                 </Split>
               </CardFooter>
             </Card>
-
           </GalleryItem>
         </Gallery>
       </PageSection>
