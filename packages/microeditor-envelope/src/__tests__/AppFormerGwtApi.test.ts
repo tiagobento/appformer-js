991bb5b4354f3ffc7f9b494e44c92b10b5f6c4a5
William Siqueira <wsiqueir@redhat.com>
2019-07-12 15:33:34 -0300

    AF-2094: Test GWT - AppFormer JS editor bridge classes

---
+++ packages/microeditor-envelope/src/__tests__/AppFormerGwtApi.test.ts
@@ -0,0 +1,33 @@
+import { AppFormerGwtApi } from '../gwt/AppFormerGwtApi';
+import { DummyEditor } from './DummyEditor';
+import * as AppFormer from "appformer-js-core";
+import { GwtEditor } from '../gwt/GwtEditor';
+
+class DummyGWTEditor {
+  private wrappedEditor: AppFormer.Editor;
+
+  constructor (wrappedEditor: AppFormer.Editor) {
+    this.wrappedEditor = wrappedEditor;
+  }
+  public get(): AppFormer.Editor {
+    return this.wrappedEditor;
+  }
+}
+
+const dummyEditor: DummyEditor = new DummyEditor();
+const dummyGWTEditor: DummyGWTEditor = new DummyGWTEditor(dummyEditor);
+const editorId:string = "dummy editor";
+const appFormerGwtApi: AppFormerGwtApi = new AppFormerGwtApi();
+
+window.gwtEditorBeans = new Map<string, { get(): GwtEditor }>();
+window.gwtEditorBeans.set(editorId, dummyGWTEditor);
+
+describe("AppFormerGwtApi", () => {
+  test("get existing editor", () => {
+    const possibleEditor = appFormerGwtApi.getEditor(editorId);
+    expect(possibleEditor).toBeTruthy();
+  });
+  test("get non existing editor", () => {
+    expect(() => appFormerGwtApi.getEditor("X")).toThrowError("GwtEditor with id 'X' was not found");
+  });
+});
---
+++ packages/microeditor-envelope/src/__tests__/GwtEditorWrapperFactory.test.ts
@@ -0,0 +1,53 @@
+import { AppFormerGwtApi } from "../gwt/AppFormerGwtApi";
+import {GwtEditorWrapperFactory} from "../gwt/GwtEditorWrapperFactory";
+import { AssertionError } from "assert";
+import { LanguageData, Resource } from "appformer-js-microeditor-router/src";
+
+const delay = (ms: number) => {
+  return new Promise(res => setTimeout(res, ms));
+};
+
+const appFormerGwtApi: AppFormerGwtApi = {
+  setErraiDomain: jest.fn(),
+  onFinishedLoading: (callback: () => Promise<any>) => window.appFormerGwtFinishedLoading = callback,
+  getEditor: jest.fn(),
+  setClientSideOnly: jest.fn()
+
+};
+const cssResource: Resource =  {
+  type: "css",
+  paths: ["resource.css"]
+};
+const jsResource: Resource = {
+  type: "js",
+  paths: ["resource.js"]
+}
+
+const testLanguageData: LanguageData = {
+  editorId: "editorID",  
+  gwtModuleName: "moduleName",
+  erraiDomain:"erraiDomain",
+  resources: [ cssResource, jsResource ]
+}
+
+const gwtEditorWrapperFactory: GwtEditorWrapperFactory = new GwtEditorWrapperFactory(appFormerGwtApi);
+
+describe("GwtEditorWrapperFactory",  () => {
+  test("create editor", async () => {
+    const res = jest.fn();
+    let links = document.getElementsByTagName('link');
+    let scripts = document.getElementsByTagName('script');
+    expect(links.length).toBe(0);
+    expect(scripts.length).toBe(0);
+    gwtEditorWrapperFactory.createEditor(testLanguageData).then(res)
+    links = document.body.getElementsByTagName('link');
+    scripts = document.getElementsByTagName('script');
+    expect(links.length).toBe(1);
+    expect(scripts.length).toBe(1);
+    expect(links[0].href).toContain(cssResource.paths[0]);
+    expect(scripts[0].src).toContain(jsResource.paths[0]);
+    window.appFormerGwtFinishedLoading();
+    await delay(100);
+    expect(res).toBeCalled();
+  });
+});
